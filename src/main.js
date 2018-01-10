8c1f0675dd105be82dd8900221b61d2dcae8ccfd
luochangchun <15872695236@163.com>
2018-01-10 09:37:29 +0800

    冲突

--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@ import Vue from 'vue'
 import api from './axios/api'
 import ElementUI from 'element-ui'
 import '../static/css/reset.css'
-import '../static/css/common.css'
 import 'element-ui/lib/theme-chalk/index.css'
-<<<<<<< HEAD
-import router from './routes_lcc.js'
-=======
+import '../static/css/common.css'
 import '../static/css/main.css'
 import router from './routes.js'
->>>>>>> ef7abfbbd50a6d26870db145571bc7f67216839c
 import App from './App'
 // import router from '../config/routes_lcc.js'
 
